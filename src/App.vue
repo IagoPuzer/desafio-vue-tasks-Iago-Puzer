2780e0e7138499df8186a69e68a0edde564fdfab
Iago Puzer <iagopuzer06@hotmail.com>
2025-11-15 12:21:11 -0300

    Chamada dos components na pagina inicial

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TaskForm from "./components/taskForm.vue";
+import TaskList from "./components/taskList.vue";
+</script>
 
 <template>
-  <div>
-    <h1>Hello Vue</h1>
+  <div class="max-w-xl mx-auto p-4 space-y-6">
+    <h1 class="text-2xl font-bold">Lista de Tarefas</h1>
+    <TaskForm />
+    <TaskList />
   </div>
 </template>
 
