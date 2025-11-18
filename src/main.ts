57401c57d7a6e1b4d575153c1dfd9642beac23f5
Iago Puzer <iagopuzer06@hotmail.com>
2025-11-18 16:03:10 -0300

    Adicionando o json-server para simular a api de tasks e usando axios para fazer as requisições

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { createPinia } from "pinia";
 import ToastPlugin from "vue-toast-notification";
 import "./style.css";
 import App from "./App.vue";
+import "./plugins/axios";
 
 createApp(App).use(createPinia()).use(ToastPlugin).mount("#app");
